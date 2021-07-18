bdcf246154433da83d2b3a6e570e091061be3056
jd <dhwnsrjedl@gmail.com>
2021-07-18 22:18:46 +0900

    Initial comit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,81 @@
 package com.company;
 
-abstract class Layer{}
-class Base extends Layer{
-    Object o;
-    Base(Object _o){
-        o = _o;
+abstract class Pizza{
+    abstract Pizza remA();
+    abstract Pizza topAwC();
+    abstract Pizza subAbC();
+}
+class Crust extends Pizza{
+    Pizza remA(){
+        return new Crust();
+    }
+    Pizza topAwC(){
+        return new Crust();
+    }
+    Pizza subAbC(){
+        return new Crust();
     }
 }
-class Slice extends Layer{
-    Layer l;
-    Slice(Layer _l){
-        l = _l;
+class Cheese extends Pizza{
+    Pizza p;
+    Cheese(Pizza _p){
+        p = _p;
+    }
+    Pizza remA(){
+        return new Cheese(p.remA());
+    }
+    Pizza topAwC(){
+        return new Cheese(p.topAwC());
+    }
+    Pizza subAbC(){
+        return new Cheese(p.subAbC());
+    }
+}
+class Olive extends Pizza{
+    Pizza p;
+    Olive(Pizza _p){
+        p = _p;
+    }
+    Pizza remA(){
+        return new Olive(p.remA());
+    }
+    Pizza topAwC(){
+        return new Olive(p.topAwC());
+    }
+    Pizza subAbC(){
+        return new Olive(p.subAbC());
+    }
+}
+class Anchovy extends Pizza{
+    Pizza p;
+    Anchovy(Pizza _p){
+        p = _p;
+    }
+    Pizza remA(){
+        return p.remA();
+    }
+    Pizza topAwC(){
+        return
+                new Cheese(
+                        new Anchovy(p.topAwC()));
+    }
+    Pizza subAbC(){
+        return new Cheese(p.subAbC());
+    }
+}
+class Sausage extends Pizza{
+    Pizza p;
+    Sausage(Pizza _p){
+        p = _p;
+    }
+    Pizza remA(){
+        return new Sausage(p.remA());
+    }
+    Pizza topAwC(){
+        return new Sausage(p.topAwC());
+    }
+    Pizza subAbC(){
+        return new Sausage(p.subAbC());
     }
 }
 public class Main {
